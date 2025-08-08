5aed1271d8795b4845e47c4d275912fcdf50f14d
cbawebdesign <35353826+cbawebdesign@users.noreply.github.com>
2025-08-08 11:45:56 -0700

    comit

--- src/components/capitalaccount/capitalaccounts.tsx
+++ src/components/capitalaccount/capitalaccounts.tsx
@@ -106,7 +106,9 @@ export default function CapitalAccounts() {
     'Capital Account 2024 Q3',
     'Capital Account 2024 Q4',
 'Final Q4 Capital Account Statement',
-'Capital Account 2025 Q1'
+'Capital Account 2025 Q1',
+'Capital Account 2025 Q2'
+
 
 
    
@@ -127,6 +129,7 @@ export default function CapitalAccounts() {
     'Capital Account 2024 Q4': 'CAS24Q4',
     'Final Q4 Capital Account Statement': 'Final Q4 CAS',
     'Capital Account 2025 Q1': 'CAS25Q1',
+    'Capital Account 2025 Q2': 'CAS25Q2',
 
 
 
