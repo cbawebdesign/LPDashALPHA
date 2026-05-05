d87b58d118e4aeadee2f3e34c66c3d9383b59a7b
cbawebdesign <35353826+cbawebdesign@users.noreply.github.com>
2026-05-05 09:36:37 -0700

    commit

--- src/components/financial/financials.tsx
+++ src/components/financial/financials.tsx
@@ -132,9 +132,14 @@ export default function Financials() {
     'Financials-Q12025',
     'Financials-Q22025',
     'Financials-Q32025',
+    'Financials-Q42025',
+
     'Quarterly Report Q1-2025',
     'Quarterly Report Q2-2025',
-    'Quarterly Report Q3-2025'
+    'Quarterly Report Q3-2025',
+    'Quarterly Report Q4-2025',
+
+
 
 
   ];
@@ -181,11 +186,14 @@ const subCategories = {
   'Quarterly Report Q2-2025': 'Quarterly Report Q2-2025',
   'Quarterly Report Q3-2025': 'Quarterly Report Q3-2025',
 
+
   'Financials-Q32024': 'Financials-Q32024',
   'Financials-Q42024': 'Financials-Q42024',
   'Financials-Q12025': 'Financials-Q12025',
   'Financials-Q22025': 'Financials-Q22025',
   'Financials-Q32025': 'Financials-Q32025',
+  'Financials-Q42025': 'Financials-Q42025',
+
 
 
 
